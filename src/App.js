fdceb2f8f093a250155513d0566594f99fd0f97e
Izyk <lance_hemphill1234@yahoo.com>
2023-07-22 18:11:26 -0400

    finished 75% of the front page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import './App.css';
 import backgroundImage1 from 'C:/Users/lance/Desktop/Developement/Coding/fashion-store/src/assets/backgroundImage1.jpg';
+import backgroundImage2 from 'C:/Users/lance/Desktop/Developement/Coding/fashion-store/src/assets/backgroundImage2.jpg';
+import backgroundImage3 from 'C:/Users/lance/Desktop/Developement/Coding/fashion-store/src/assets/backgroundImage3.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const App = () => {
+
+    AOS.init({duration: 1000});
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <div>
-            <div>
-                <img src={backgroundImage1} alt='Background'/>
+            <div className='navbarContainer'>
+                <ul className='navbar'>
+                    <li>Placeholder</li>
+                    <li>Placeholder</li>
+                    <li>Placeholder</li>
+                    <li>Placeholder</li>
+                </ul>
+            </div>
+            <div className='backgroundImageContainer'>
+                <img className='backgroundImage1' src={backgroundImage1} alt='Background'/>
+                <img className='backgroundImage2' src={backgroundImage2} alt='Background'/>
+                <img className='backgroundImage3' src={backgroundImage3} alt='background'/>
+            </div>
+            <div className='headerContainer'>
+                <p data-aos='fade-up' className='siteHeader'>Mode</p>
             </div>
         </div>
     );
