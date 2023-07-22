8606bdabc8a009f9823689b39a996070d989ce11
Izyk <lance_hemphill1234@yahoo.com>
2023-07-22 16:58:54 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import backgroundImage1 from 'C:/Users/lance/Desktop/Developement/Coding/fashion-store/src/assets/backgroundImage1.jpg';
+
+const App = () => {
+    return (
+        <div>
+            <div>
+                <img src={backgroundImage1} alt='Background'/>
+            </div>
+        </div>
+    );
+};
+
+export default App;
